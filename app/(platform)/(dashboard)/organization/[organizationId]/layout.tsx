2f83c12a6db3756e74adec3db858073191eeaf2a
arpit jana <arpitjana2103@gmail.com>
2026-08-11 14:26:09 +0530

    feat : showing orgName on pageTitle

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,10 +1,37 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import type { Metadata } from "next";
+
 import OrgControl from "./_components/org-control";
 
 type TOrganizationLayoutProps = {
     children: React.ReactNode;
+    params: Promise<{ organizationId: string }>;
 };
 
-export default function OrganizationIdLayout({ children }: TOrganizationLayoutProps) {
+export async function generateMetadata({
+    params,
+}: {
+    params: Promise<{ organizationId: string }>;
+}): Promise<Metadata> {
+    const { organizationId } = await params;
+    const client = await clerkClient();
+
+    try {
+        const organization = await client.organizations.getOrganization({
+            organizationId,
+        });
+
+        return {
+            title: organization.name,
+        };
+    } catch {
+        return {
+            title: "Organzation",
+        };
+    }
+}
+
+export default async function OrganizationIdLayout({ children }: TOrganizationLayoutProps) {
     return (
         <>
             <OrgControl />
